59010c8cb0690015e3abfed3bdb0e051cdb54040
Rayen03 <rayenhassen00@gmail.com>
2023-11-06 20:59:05 +0100

    deleted redundant files

--- client/src/store/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { counterSlice } from "./features/CounterSlice";
-
-export const store = configureStore({
-  reducer: { counterReducer: counterSlice },
-});
